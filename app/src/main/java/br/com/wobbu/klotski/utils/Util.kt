ff98fc64f23f8e9a13934dfaa5da9f79e5b1976d
Eduardo Ewerton <edussj3@gmail.com>
2019-07-15 20:02:05 +0100

    Added click to start

---
+++ app/src/main/java/br/com/wobbu/klotski/utils/Util.kt
@@ -0,0 +1,15 @@
+package br.com.wobbu.klotski.utils
+
+import android.content.Context
+
+object Util {
+    fun dp2px(context: Context, dpValue: Float): Int {
+        val scale = context.resources.displayMetrics.density
+        return (dpValue * scale + 0.5f).toInt()
+    }
+
+    fun px2dp(context: Context, pxValue: Float): Int {
+        val scale = context.resources.displayMetrics.density
+        return (pxValue / scale + 0.5f).toInt()
+    }
+}
